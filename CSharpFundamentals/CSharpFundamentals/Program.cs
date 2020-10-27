4758c83586e4ffb460ed69767d6d86d7be636d6a
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-10-27 17:40:31 +0100

    CSharpFundamentals added

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Xml;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] {"Jack","John","Mary"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+
+            var c = 10;
+            var d = c;
+            d++;
+            Console.WriteLine(string.Format("c: {0}, d:{1}",c,d));
+
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1;
+            Console.WriteLine(string.Format("array1[]0: {0}, array2[0] {1}", array1[0], array2[0]));
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[]0: {0}, array2[0] {1}", array1[0], array2[0]));
+
+        }
+    }
+}
