3b8e8a5582fce2edaceb9ebdc8a432f00a5e62df
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-10-27 17:42:55 +0100

    Operators/RandomClass/TypeConversion added

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            var x = true;
+
+            var y = false;
+
+            var z = true && false;
+            Console.WriteLine(z);
+        }
+    }
+}
