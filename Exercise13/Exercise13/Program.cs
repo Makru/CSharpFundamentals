6fe19a8c1687b465a96d39778277321034a2001f
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-02 18:48:06 +0100

    Erxercise 13-14 und DateTime Demo added

---
+++ Exercise13/Exercise13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise13
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Bitte enter eine Nummer oder enter Quit zum Beenden!");
+                var input = Console.ReadLine();
+
+                if (input != "Quit")
+                {
+                    numbers.Add(Convert.ToInt32(input));
+                    continue;
+                }
+
+                break;
+
+            }
+            //Gibt die gesamte Liste aus
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Gibt ein neue Liste mit den "unique" Zahlen aus der vorherigen Liste aus
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+
+            Console.WriteLine("Unique numbers:");
+            foreach (var number in uniques)
+                Console.WriteLine(number);
+        }
+    }
+}
---
+++ Exercise14/Exercise14/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bitte gebe eine Liste mit Zahlen durch Komma getrennt ein:");
+
+                var input = Console.ReadLine();
+                var numbers = input.Split(',');
+                Console.WriteLine("stringLength of input: " + input.Length);
+                Console.WriteLine("stringLength of numbers: " + numbers.Length);
+                Console.WriteLine();
+                Console.WriteLine("String-Liste:");
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+
+                if (input.Length == 0)
+                {
+                    Console.WriteLine("Bitte erneut versuchen!");
+                    continue;
+                }
+
+                else
+                {
+
+
+                    var zahlen = new List<int>();
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        var listItem = Convert.ToInt32(numbers[i]);
+                        zahlen.Add(listItem);
+                    }
+
+                    Console.WriteLine("Int-Liste:");
+                    foreach (var zahl in zahlen)
+                    {
+                        Console.WriteLine(zahl);
+                    }
+                    zahlen.Sort();
+                    Console.WriteLine("Sortierte Liste der 3 kleinsten Zahlen: ");
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.WriteLine(zahlen[k]);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
