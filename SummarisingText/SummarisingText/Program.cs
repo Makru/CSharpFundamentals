8c914941a5e6dbdf474a7930aad6986c0def0779
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-03 09:00:28 +0100

    String & SummarisingText Exercise added

---
+++ SummarisingText/SummarisingText/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            var sentence2 = "This is going to be.";
+
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
+            var summary2 = StringUtility.SummerizeText(sentence2, 5);
+            Console.WriteLine(summary2);
+        }
+
+   
+    }
+}
