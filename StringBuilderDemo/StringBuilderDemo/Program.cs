1dacd47a0e2b6729306496a9397b484fa926c0e9
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-05 18:11:56 +0100

    StringBuilderDemo added

---
+++ StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .AppendLine("Header")
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("Erster Buchstabe:" + builder[0]);
+
+        }
+    }
+}
