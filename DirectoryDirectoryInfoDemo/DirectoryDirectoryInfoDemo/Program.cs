d817b1396d48649ca0309dd004322685e7b75cb9
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-06 17:53:19 +0100

    File & Directory Demo added

---
+++ DirectoryDirectoryInfoDemo/DirectoryDirectoryInfoDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryDirectoryInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\temp\folder1");
+
+            //var files = Directory.GetFiles(@"C:\Users\mathi\source\repos\CSharp_Fundamentals", "*.sln",
+            //    SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file);
+            //}
+
+            var directories = Directory.GetDirectories(@"C:\Users\mathi\source\repos\CSharp_Fundamentals", "*.*",
+                SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists(@"C:\Users\mathi\source\repos\CSharp_Fundamentals");
+
+            var directoryInfo = new DirectoryInfo(@"C:\Users\mathi\source\repos\CSharp_Fundamentals");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
