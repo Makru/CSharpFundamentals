21a7508a017345faa2c9f012b9703c38976a6e10
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-06 17:56:07 +0100

    Exercise 20 & 21 added

---
+++ Exercise20/Exercise20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var path = @"C:\Users\mathi\source\repos\Exercise20\myfile.txt";
+            var content = File.ReadAllText(path);
+            var count = (content.Split(" ")).Length;
+            
+            Console.WriteLine(content);
+            Console.WriteLine("Die Anzahl der Wörter in myfile.txt ist: " + count);
+
+        }
+    }
+}
---
+++ Exercise21/Exercise21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var path = @"C:\Users\mathi\source\repos\Exercise20\myfile.txt";
+            var content = File.ReadAllText(path);
+            var count = (content.Split(" ")).Length;
+
+            Console.WriteLine(content);
+            Console.WriteLine("Die Anzahl der Wörter in myfile.txt ist: " + count);
+
+            string word = "";
+            int ctr = 0;
+            foreach (String str in (content.Split(" ")))
+            {
+                if (str.Length > ctr)
+                {
+                    word = str;
+                    ctr = str.Length;
+                }
+            }
+
+            Console.WriteLine("Das längste Wort in myfile.txt ist: "+ word);
+
+        }
+    }
+}
