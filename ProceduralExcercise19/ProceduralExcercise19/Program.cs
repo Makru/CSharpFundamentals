a107721369bc5a17141acd4db7653fa294089107
Mathias Krueger <mathias.krueger@sunrise.ch>
2020-11-06 14:42:18 +0100

    ProceduralProgramming und Exercises added

---
+++ ProceduralExcercise19/ProceduralExcercise19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter one or more English words: ");
+            var word = Console.ReadLine();
+
+            Console.WriteLine("The number of vowels is: {0}", GetVowelsCount(word));
+        }
+
+        public static int GetVowelsCount(string word)
+        {
+            int total = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                {
+                    total++;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ ProceduralProgramming/ProceduralProgramming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+            var reversedName = ReverseName(name);
+            Console.WriteLine("Reversed Name: " + reversedName);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+        }
+    }
+}
